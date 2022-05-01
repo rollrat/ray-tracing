e8050831e832e220d3c22bb26203ff5b1916f550
rollrat <rollrat.cse@gmail.com>
2022-05-01 21:48:29 +0900

    lecture 7. apply anti aliasing

--- vec3.h
+++ vec3.h
@@ -224,4 +224,75 @@ inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180.0;
 }
 
+#include <cstdlib>
+#include <random>
+
+// inline double random_double() {
+//   // Returns a random real in [0,1).
+//   return rand() / (RAND_MAX + 1.0);
+// }
+
+inline double random_double() {
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  static std::mt19937 generator;
+  return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+  // Returns a random real in [min,max).
+  return min + (max - min) * random_double();
+}
+
+class camera {
+public:
+  camera() {
+    auto aspect_ratio = 16.0 / 9.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    origin = point3(0, 0, 0);
+    horizontal = vec3(viewport_width, 0.0, 0.0);
+    vertical = vec3(0.0, viewport_height, 0.0);
+    lower_left_corner =
+        origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+  }
+
+  ray get_ray(double u, double v) const {
+    return ray(origin,
+               lower_left_corner + u * horizontal + v * vertical - origin);
+  }
+
+private:
+  point3 origin;
+  point3 lower_left_corner;
+  vec3 horizontal;
+  vec3 vertical;
+};
+
+inline double clamp(double x, double min, double max) {
+  if (x < min)
+    return min;
+  if (x > max)
+    return max;
+  return x;
+}
+
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
+  auto r = pixel_color.x();
+  auto g = pixel_color.y();
+  auto b = pixel_color.z();
+
+  // Divide the color by the number of samples.
+  auto scale = 1.0 / samples_per_pixel;
+  r *= scale;
+  g *= scale;
+  b *= scale;
+
+  // Write the translated [0,255] value of each color component.
+  out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+      << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+      << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
+}
+
 #endif
